54135e5d3a081e9b711c6fbb9222b070f8273d70
Juchli Bruno Lorenz <bruno.lorenz.juchli@erowa.com>
2020-02-10 15:15:02 +0100

    Moving the file. Opening it in visual studio and saving it.
    --> The namespace changes!

--- WithResources/Foo/Example.Designer.cs
+++ WithResources/Foo/Example.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WithResources.Foo.Bar {
+namespace WithResources.Foo {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WithResources.Foo.Bar {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WithResources.Foo.Bar.Example", typeof(Example).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WithResources.Foo.Example", typeof(Example).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
